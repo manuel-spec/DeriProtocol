a404fbde6f5a3e5d62153aaf045b0cf2ed72d0a1
manuel-spec <amaniase084@gmail.com>
2024-03-25 04:58:31 -0700

    crypto table switcher

--- src/components/HomeComponent/Table.jsx
+++ src/components/HomeComponent/Table.jsx
@@ -15,6 +15,9 @@ const columns = [
 
 const StickyHeadTable = () => {
   const [fetchedData, setFetchedData] = useState([]); // State to hold fetched data
+  const [losersfetchedData, setLosersFetchedData] = useState([]);
+  const [normalfetchedData, set24FetchedData] = useState([]);
+
   const [loading, setLoading] = useState(true); // State to track loading status
   const [activeComponent, setActiveComponent] = useState("Gainers");
 
@@ -38,8 +41,6 @@ const StickyHeadTable = () => {
           symbol: coin.image, // Add symbol for fetching logo
         }));
 
-        Gainers.sort((a, b) => b["24H"] - a["24H"]);
-
         // trending coins
 
         const coins = response.data.coins.map((coin) => ({
@@ -50,7 +51,13 @@ const StickyHeadTable = () => {
           symbol: coin.item.small, // Add symbol for fetching logo
         }));
 
-        setFetchedData(Gainers.slice(0, 15));
+        setFetchedData(
+          Gainers.slice(0, 15).sort((a, b) => b["24H"] - a["24H"])
+        );
+        setLosersFetchedData(
+          Gainers.slice(0, 15).sort((a, b) => a["24H"] - b["24H"])
+        );
+        set24FetchedData(Gainers.slice(0, 15));
         setLoading(false); // Set loading to false after data is fetched
       } catch (error) {
         setLoading(false); // Set loading to false in case of error
@@ -64,10 +71,9 @@ const StickyHeadTable = () => {
     <div className="overflow-hidden max-h-440 rounded-lg shadow">
       <div className="flex flex-row justify-between items-center bg-[#0F1720] w-full text-sm mt-2">
         <button
-          className="text-white p-2 ml-3"
+          className="text-white p-2 ml-3 focus:border"
           onClick={() => {
-            fetchedData.sort((a, b) => b["24H"] - a["24H"]);
-            console.log(1);
+            setActiveComponent("Gainers");
           }}
         >
           Top Gainers
@@ -80,7 +86,14 @@ const StickyHeadTable = () => {
         >
           Top Losers
         </button>
-        <button className="text-white p-2 mr-2">24H Turn Over</button>
+        <button
+          className="text-white p-2 mr-2"
+          onClick={() => {
+            setActiveComponent("24");
+          }}
+        >
+          24H Turn Over
+        </button>
       </div>
 
       {activeComponent == "Gainers" && (
@@ -136,7 +149,116 @@ const StickyHeadTable = () => {
           </tbody>
         </table>
       )}
-      {/* Pagination component can be added here */}
+
+      {/* losers table component */}
+      {activeComponent == "Losers" && (
+        <table className="w-full border-collapse">
+          <thead className="bg-gray-800 text-white">
+            {" "}
+            <tr>
+              {columns.map((column) => (
+                <th
+                  key={column.id}
+                  className="px-4 py-2 text-left" // Text styling
+                >
+                  <p className="text-xs font-normal"> {column.label}</p>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="bg-gray-900 text-white">
+            {loading ? ( // Check loading state before rendering data
+              <tr key="loading">
+                <td colSpan={columns.length} className="px-4 py-2 text-center">
+                  Loading...
+                </td>
+              </tr>
+            ) : (
+              losersfetchedData.map((row, index) => (
+                <tr key={index} className="hover:bg-gray-800">
+                  {" "}
+                  {/* Hover effect */}
+                  <td className="px-4 py-2 flex items-center">
+                    <img src={row.symbol} alt="" width={30} className="mr-2" />
+                    <span className="font-bold text-sm">{row.coin} </span>
+                    <span className="text-xs">/ USDT</span>
+                  </td>
+                  <td className="px-4 py-2 text-sm">
+                    ${parseFloat(row.price).toFixed(3)}
+                  </td>
+                  <td className="px-4 py-2 text-right text-sm">
+                    <div
+                      style={{
+                        backgroundColor: row["24H"] > 0 ? "#04CF99" : "#F36464",
+                      }}
+                      className="rounded text-center"
+                    >
+                      <span className="px-2 py-1  text-sm">
+                        {row["24H"].toFixed(2)}
+                      </span>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      )}
+
+      {/* 24 table component */}
+      {activeComponent == "24" && (
+        <table className="w-full border-collapse">
+          <thead className="bg-gray-800 text-white">
+            {" "}
+            <tr>
+              {columns.map((column) => (
+                <th
+                  key={column.id}
+                  className="px-4 py-2 text-left" // Text styling
+                >
+                  <p className="text-xs font-normal"> {column.label}</p>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="bg-gray-900 text-white">
+            {loading ? ( // Check loading state before rendering data
+              <tr key="loading">
+                <td colSpan={columns.length} className="px-4 py-2 text-center">
+                  Loading...
+                </td>
+              </tr>
+            ) : (
+              normalfetchedData.map((row, index) => (
+                <tr key={index} className="hover:bg-gray-800">
+                  {" "}
+                  {/* Hover effect */}
+                  <td className="px-4 py-2 flex items-center">
+                    <img src={row.symbol} alt="" width={30} className="mr-2" />
+                    <span className="font-bold text-sm">{row.coin} </span>
+                    <span className="text-xs">/ USDT</span>
+                  </td>
+                  <td className="px-4 py-2 text-sm">
+                    ${parseFloat(row.price).toFixed(3)}
+                  </td>
+                  <td className="px-4 py-2 text-right text-sm">
+                    <div
+                      style={{
+                        backgroundColor: row["24H"] > 0 ? "#04CF99" : "#F36464",
+                      }}
+                      className="rounded text-center"
+                    >
+                      <span className="px-2 py-1  text-sm">
+                        {row["24H"].toFixed(2)}
+                      </span>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
