f8ec4e2ea0179f7b564fb3d1c12c184511bbd643
manuel-spec <amaniase084@gmail.com>
2024-03-27 09:48:04 -0700

    exchange component done

--- src/components/Assets/Exchange.jsx
+++ src/components/Assets/Exchange.jsx
@@ -44,14 +44,14 @@ const Exchange = () => {
               <p className="text-white font-semibold ml-2">USDT</p>
             </div>
             <div>
-              <p className="text-sm text-[#ddd] font-semibold">
+              <p className="text-sm text-[#A7A8A9] font-semibold">
                 Valuation ≈$ 0.00
               </p>
             </div>
           </div>
           <div className="flex flex-row text-sm justify-between text-white p-4 items-center">
-            <div className="text-[#ddd] text-xs ml-2">Freeze 0</div>
-            <div className="text-[#ddd] text-xs">Availabe 0</div>
+            <div className="text-[#A7A8A9] text-xs ml-2">Freeze 0</div>
+            <div className="text-[#A7A8A9] text-xs">Availabe 0</div>
           </div>
         </div>
         <div className="flex flex-col border-b-2 border-black mb-2">
@@ -65,14 +65,14 @@ const Exchange = () => {
               <p className="text-white font-semibold ml-2">ETH</p>
             </div>
             <div>
-              <p className="text-sm text-[#ddd] font-semibold">
+              <p className="text-sm text-[#A7A8A9] font-semibold">
                 Valuation ≈$ 0.00
               </p>
             </div>
           </div>
           <div className="flex flex-row text-sm justify-between text-white p-4 items-center">
-            <div className="text-[#ddd] text-xs ml-2">Freeze 0</div>
-            <div className="text-[#ddd] text-xs">Availabe 0</div>
+            <div className="text-[#A7A8A9] text-xs ml-2">Freeze 0</div>
+            <div className="text-[#A7A8A9] text-xs">Availabe 0</div>
           </div>
         </div>
         <div className="flex flex-col border-b-2 border-black mb-2">
@@ -86,14 +86,14 @@ const Exchange = () => {
               <p className="text-white font-semibold ml-2">BTC</p>
             </div>
             <div>
-              <p className="text-sm text-[#ddd] font-semibold">
+              <p className="text-sm text-[#A7A8A9] font-semibold">
                 Valuation ≈$ 0.00
               </p>
             </div>
           </div>
           <div className="flex flex-row text-sm justify-between text-white p-4 items-center">
-            <div className="text-[#ddd] text-xs ml-2">Freeze 0</div>
-            <div className="text-[#ddd] text-xs">Availabe 0</div>
+            <div className="text-[#A7A8A9] text-xs ml-2">Freeze 0</div>
+            <div className="text-[#A7A8A9] text-xs">Availabe 0</div>
           </div>
         </div>
         <div className="flex flex-col border-b-2 border-black mb-2">
@@ -107,28 +107,36 @@ const Exchange = () => {
               <p className="text-white font-semibold ml-2">USDC</p>
             </div>
             <div>
-              <p className="text-sm text-[#ddd] font-semibold">
+              <p className="text-sm text-[#A7A8A9] font-semibold">
                 Valuation ≈$ 0.00
               </p>
             </div>
           </div>
           <div className="flex flex-row text-sm justify-between text-white p-4 items-center">
-            <div className="text-[#ddd] text-xs ml-2">Freeze 0</div>
-            <div className="text-[#ddd] text-xs">Availabe 0</div>
+            <div className="text-[#A7A8A9] text-xs ml-2">Freeze 0</div>
+            <div className="text-[#A7A8A9] text-xs">Availabe 0</div>
           </div>
         </div>
-
-        <div>
-          <img
-            src="https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            src="https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png"
-            alt=""
-          />
+        <div className="flex flex-col border-b-2 border-black mb-2">
+          <div className="flex flex-row justify-between items-center  text-white">
+            <div className="flex flex-row mb-1 ml-2 mt-2">
+              <img
+                src="https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png"
+                alt=""
+                width={25}
+              />
+              <p className="text-white font-semibold ml-2">DAI</p>
+            </div>
+            <div>
+              <p className="text-sm text-[#A7A8A9] font-semibold">
+                Valuation ≈$ 0.00
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row text-sm justify-between text-white p-4 items-center mb-10">
+            <div className="text-[#A7A8A9] text-xs ml-2">Freeze 0</div>
+            <div className="text-[#A7A8A9] text-xs">Availabe 0</div>
+          </div>
         </div>
       </div>
     </div>
