3d66380244d8efddef88c856415c4033e4138a32
manuel-spec <amaniase084@gmail.com>
2024-03-31 00:47:14 -0700

    bottom navigation style

--- src/components/BottomNavigationBar.jsx
+++ src/components/BottomNavigationBar.jsx
@@ -1,12 +1,13 @@
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
-import TrendingUpOutlinedIcon from "@mui/icons-material/TrendingUpOutlined";
-import CurrencyExchangeOutlinedIcon from "@mui/icons-material/CurrencyExchangeOutlined";
-import LoopIcon from "@mui/icons-material/Loop";
-import WalletIcon from "@mui/icons-material/Wallet";
 import { useState } from "react";
 import { createTheme } from "@mui/material/styles";
 import { Link } from "react-router-dom";
+import home from "../assets/icons/nav/home.png";
+import spot from "../assets/icons/nav/spot.png";
+import trading from "../assets/icons/nav/trading.png";
+import perpetual from "../assets/icons/nav/perpetual.png";
+import assets from "../assets/icons/nav/assets.png";
+import { useLocation } from "react-router-dom";
 
 const darkTheme = createTheme({
   palette: {
@@ -16,7 +17,8 @@ const darkTheme = createTheme({
 
 const BottomNavigationBar = () => {
   const [value, setValue] = useState(0);
-
+  const location = useLocation();
+  console.log("Current URL:", location.pathname);
   return (
     <div>
       <BottomNavigation
@@ -29,38 +31,53 @@ const BottomNavigationBar = () => {
           setValue(newValue);
         }}
       >
-        <Link to="/" className="text-white flex flex-col justify-center p-2">
-          <HomeIcon style={{ color: "#C0C0C0" }} className="" />
-          <p className="text-xs text-center">Home</p>
-        </Link>
-        <Link
-          to="/spot"
-          className="text-white flex flex-col justify-center p-2"
-        >
-          <TrendingUpOutlinedIcon style={{ color: "#C0C0C0" }} />
-          <p className="text-xs text-center">Spot</p>
-        </Link>
-        <Link
-          to="/trade"
-          className="text-white flex flex-col justify-center p-2"
-        >
-          <CurrencyExchangeOutlinedIcon style={{ color: "#C0C0C0" }} />
-          <p className="text-xs text-center">Trading</p>
-        </Link>
-        <Link
-          to="/perpetual"
-          className="text-white flex flex-col justify-center p-2"
-        >
-          <LoopIcon style={{ color: "#C0C0C0" }} />
-          <p className="text-xs text-center">Perpetual</p>
-        </Link>
-        <Link
-          to="/assets"
-          className="text-white flex flex-col justify-center p-2"
-        >
-          <WalletIcon style={{ color: "#C0C0C0" }} />
-          <p className="text-xs text-center">Assets</p>
-        </Link>
+        <div className="flex flex-row justify-between items-center">
+          <div className="p-3">
+            <Link
+              to="/"
+              className="text-white flex flex-col justify-center p-2"
+            >
+              <img src={home} alt="" width={25} className="ml-1" />
+              <p className="text-xs text-center">Home</p>
+            </Link>
+          </div>
+          <div className="p-3">
+            <Link
+              to="/spot"
+              className="text-white flex flex-col justify-center p-2"
+            >
+              <img src={spot} alt="" width={25} className="ml-1" />
+              <p className="text-xs text-center">Spot</p>
+            </Link>
+          </div>
+          <div className="p-3">
+            <Link
+              to="/trade"
+              className="text-white flex flex-col justify-center p-2"
+            >
+              <img src={trading} alt="" width={25} className="ml-1" />
+              <p className="text-xs text-center">Trading</p>
+            </Link>
+          </div>
+          <div className="p-3">
+            <Link
+              to="/perpetual"
+              className="text-white flex flex-col justify-center p-2"
+            >
+              <img src={perpetual} alt="" width={25} className="ml-3" />
+              <p className="text-xs text-center">Perpetual</p>
+            </Link>
+          </div>
+          <div className="p-3">
+            <Link
+              to="/assets"
+              className="text-white flex flex-col justify-center p-2"
+            >
+              <img src={assets} alt="" width={25} className="ml-1" />
+              <p className="text-xs text-center">Assets</p>
+            </Link>
+          </div>
+        </div>
       </BottomNavigation>
     </div>
   );
