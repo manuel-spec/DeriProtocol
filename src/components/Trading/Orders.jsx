59b56bb5830d0966ad343547bddd306265f9d251
manuel-spec <amaniase084@gmail.com>
2024-04-08 09:55:59 -0700

    order in progress done

--- src/components/Trading/Orders.jsx
+++ src/components/Trading/Orders.jsx
@@ -3,6 +3,7 @@ import nodata from "../../assets/imgs/nodata.png";
 import Axios from "axios";
 import Cookies from "universal-cookie";
 import { jwtDecode } from "jwt-decode";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 
 const Orders = () => {
   const [orders, setOrders] = useState();
@@ -69,21 +70,31 @@ const Orders = () => {
           </div>
         </div>
       )}
-      {orders && (
+      {orders && activeButton == "orders" && (
         <div className="mb-20">
           {orders.map((item, index) => (
-            <div key={index} className="text-white p-2 ml-3 text-sm">
+            <div
+              key={index}
+              className="flex flex-row justify-between align-center text-white p-2 ml-3 text-sm"
+            >
               <div className="flex flex-col">
                 <div className="flex flex-row">
-                  <p>token name:</p>
+                  <p>Token Name:</p>
                   <p className="text-sm">{item.token_name}</p>
                 </div>
                 <div className="flex flex-row">
-                  <p className="text-sm">trade amount:</p>
+                  <p className="text-sm">Trade amount:</p>
                   <p className="text-sm ml-2">
                     {item.trade_amount}
                     {item.token_name}
                   </p>
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <div className="text-sm">Trade time</div>
+                <div>
+                  <AccessAlarmIcon className="mr-3" />
+                  {item.trade_percent}
                 </div>
               </div>
             </div>
